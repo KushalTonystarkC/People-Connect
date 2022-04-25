e42506c0718d8f1587775fc8bee2337ec2af7f3c
KUSHAL SAVIT CHOUDHARY <74146269+KushalTonystarkC@users.noreply.github.com>
2022-04-25 13:10:21 +0530

    First Commit

---
+++ People Connect/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCnRS6pAERMR2M5iaYIL7UpjwDhvTYke5g",
+    authDomain: "maithili-connect.firebaseapp.com",
+    databaseURL: "https://maithili-connect-default-rtdb.firebaseio.com",
+    projectId: "maithili-connect",
+    storageBucket: "maithili-connect.appspot.com",
+    messagingSenderId: "325687209498",
+    appId: "1:325687209498:web:b38333071e60205d81eba3",
+    measurementId: "G-BDJBYBG0YG"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
